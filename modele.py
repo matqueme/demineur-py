7a1e00f403f1a1241cee2e86057dfa16bff96998
matqueme <mathis.quemener@gmail.com>
2023-01-18 10:02:18 +0100

    Creation d'une nouvelles classe pour les fonctions

--- modele.py
+++ modele.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import utils
 
 
 class Generation:
@@ -12,51 +13,9 @@ class Generation:
         self.array = np.zeros(shape=(self.column, self.row), dtype=object)
         self.arrayHide = np.full((self.column, self.row), '*', dtype=object)
 
-        # genere un tableau avec toutes les valeurs ou l'on ne peut pas mettre de bombes, cad 3x3 autour de la génération
+        self.utils = utils.Utils()
 
-        def impossible_array(i, j):
-            total = [[i, j]]
-            if j > 0:
-                total.append([i, (j-1)])
-            if i > 0:
-                total.append([(i-1), j])
-            if i < 15:
-                total.append([(i+1), j])
-            if j < 15:
-                total.append([i, (j+1)])
-            if i > 0 and j > 0:
-                total.append([(i-1), (j-1)])
-            if i < 15 and j < 15:
-                total.append([(i+1), (j+1)])
-            if i > 0 and j < 15:
-                total.append([(i-1), (j+1)])
-            if i < 15 and j > 0:
-                total.append([(i+1), (j-1)])
-            return total
-
-        # verifie le nombre de bombe a une coordonnée precise
-        def get_adjacent_numbers(grid, i, j):
-            total = []
-            if j > 0:
-                total.append(grid[i][(j-1)])
-            if i > 0:
-                total.append(grid[(i-1)][j])
-            if i < 15:
-                total.append(grid[(i+1)][j])
-            if j < 15:
-                total.append(grid[i][(j+1)])
-            if i > 0 and j > 0:
-                total.append(grid[(i-1)][(j-1)])
-            if i < 15 and j < 15:
-                total.append(grid[(i+1)][(j+1)])
-            if i > 0 and j < 15:
-                total.append(grid[(i-1)][(j+1)])
-            if i < 15 and j > 0:
-                total.append(grid[(i+1)][(j-1)])
-            return str(total.count('b'))
-
-        arrayImposible = impossible_array(xClick, yClick)
-        print(arrayImposible)
+        arrayImposible = self.utils.impossible_array(xClick, yClick)
         # genere des bombes dès que l'on est pas sur une case impossible ou que le il n'y a pas de bombe
         tempNbBomb = 0
         while tempNbBomb != self.nbBomb:
@@ -64,7 +23,6 @@ class Generation:
             x = random.randint(0, row-1)
             y = random.randint(0, column-1)
             for i in range(len(arrayImposible)):
-                print([y, x])
                 if arrayImposible[i] == [x, y]:
                     erreur = 1
             if self.array[x][y] == 'b':
@@ -77,42 +35,11 @@ class Generation:
         for x in range(len(self.array)):
             for y in range(len(self.array[0])):
                 if (self.array[x][y] != 'b'):
-                    self.array[x][y] = get_adjacent_numbers(self.array, x, y)
-
-        #a = (self.array == 'b').sum()
-
-        print(self.array)
+                    self.array[x][y] = self.utils.get_adjacent_numbers(
+                        self.array, x, y)
         self.deleteCase(self.xClick, self.yClick)
 
     def deleteCase(self, x, y):
-
-        def get_adjacent_numbers(grid, i, j):
-            total = []
-            if j > 0:
-                if (grid[j-1][i] == '0'):
-                    total.append([i, (j-1)])
-            if i > 0:
-                if (grid[j][i-1] == '0'):
-                    total.append([i-1, j])
-            if i < 15:
-                if (grid[j][i+1] == '0'):
-                    total.append([(i+1), j])
-            if j < 15:
-                if (grid[j+1][i] == '0'):
-                    total.append([i, j+1])
-            if i > 0 and j > 0:
-                if (grid[(j-1)][(i-1)] == '0'):
-                    total.append([(i-1), (j-1)])
-            if i < 15 and j < 15:
-                if (grid[(j+1)][(i+1)] == '0'):
-                    total.append([(i+1), (j+1)])
-            if i > 0 and j < 15:
-                if (grid[(j+1)][(i-1)] == '0'):
-                    total.append([(i-1), (j+1)])
-            if i < 15 and j > 0:
-                if (grid[(j-1)][(i+1)] == '0'):
-                    total.append([(i+1), (j-1)])
-            return total
 
         def add_number(i, j):
             self.arrayHide[j][i] = (self.array[j][i])
@@ -139,26 +66,20 @@ class Generation:
             getZeroTab = [[x, y]]
             index = 0
             while (len(getZeroTab) > index):
-                tabtemp = get_adjacent_numbers(
+                tabtemp = self.utils.get_adjacent_numbers(
                     self.array, getZeroTab[index][0], getZeroTab[index][1])
                 for i in range(len(tabtemp)):
                     if tabtemp[i] not in getZeroTab:
                         getZeroTab.append(
                             tabtemp[i])
                 index += 1
-
             for i in range(len(getZeroTab)):
                 add_number(getZeroTab[i][0], getZeroTab[i][1])
-
-            print(self.arrayHide)
-            print("final", getZeroTab, len(getZeroTab))
         else:
             self.arrayHide[y][x] = self.array[y][x]
-            print(self.arrayHide)
+            return self.arrayHide
 
 
-Demineur = Generation(16, 16, 40, 0, 5)
-Demineur.deleteCase(5, 10)
 '''question : 
 - Le test autour de chaque nombre avec tout les if c'est bien ?
 - Faire un tableau avec la boucle pour les valeurs interditent c'est bien ou autant mettre en statique ?
---
+++ utils.py
@@ -0,0 +1,71 @@
+class Utils:
+    # genere un tableau avec toutes les valeurs ou l'on ne peut pas mettre de bombes, cad 3x3 autour de la génération
+
+    def impossible_array(self, i, j):
+        total = [[i, j]]
+        if j > 0:
+            total.append([i, (j-1)])
+        if i > 0:
+            total.append([(i-1), j])
+        if i < 15:
+            total.append([(i+1), j])
+        if j < 15:
+            total.append([i, (j+1)])
+        if i > 0 and j > 0:
+            total.append([(i-1), (j-1)])
+        if i < 15 and j < 15:
+            total.append([(i+1), (j+1)])
+        if i > 0 and j < 15:
+            total.append([(i-1), (j+1)])
+        if i < 15 and j > 0:
+            total.append([(i+1), (j-1)])
+        return total
+        # verifie le nombre de bombe a une coordonnée precise
+
+    def get_adjacent_numbers(self, grid, i, j):
+        total = []
+        if j > 0:
+            total.append(grid[i][(j-1)])
+        if i > 0:
+            total.append(grid[(i-1)][j])
+        if i < 15:
+            total.append(grid[(i+1)][j])
+        if j < 15:
+            total.append(grid[i][(j+1)])
+        if i > 0 and j > 0:
+            total.append(grid[(i-1)][(j-1)])
+        if i < 15 and j < 15:
+            total.append(grid[(i+1)][(j+1)])
+        if i > 0 and j < 15:
+            total.append(grid[(i-1)][(j+1)])
+        if i < 15 and j > 0:
+            total.append(grid[(i+1)][(j-1)])
+        return str(total.count('b'))
+
+    def get_adjacent_numbers(grid, i, j):
+        total = []
+        if j > 0:
+            if (grid[j-1][i] == '0'):
+                total.append([i, (j-1)])
+        if i > 0:
+            if (grid[j][i-1] == '0'):
+                total.append([i-1, j])
+        if i < 15:
+            if (grid[j][i+1] == '0'):
+                total.append([(i+1), j])
+        if j < 15:
+            if (grid[j+1][i] == '0'):
+                total.append([i, j+1])
+        if i > 0 and j > 0:
+            if (grid[(j-1)][(i-1)] == '0'):
+                total.append([(i-1), (j-1)])
+        if i < 15 and j < 15:
+            if (grid[(j+1)][(i+1)] == '0'):
+                total.append([(i+1), (j+1)])
+        if i > 0 and j < 15:
+            if (grid[(j+1)][(i-1)] == '0'):
+                total.append([(i-1), (j+1)])
+        if i < 15 and j > 0:
+            if (grid[(j-1)][(i+1)] == '0'):
+                total.append([(i+1), (j-1)])
+        return total
